de38d950a11c4f7e34e24f5420555e0727862618
zeherb <bouzid.zeher@gmail.com>
2021-09-22 14:37:48 +0100

    edit profile

---
+++ src/app/my-components/my-profile/dialogs/edit-avatar/edit-avatar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-avatar',
+  templateUrl: './edit-avatar.component.html',
+  styleUrls: ['./edit-avatar.component.css']
+})
+export class EditAvatarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
